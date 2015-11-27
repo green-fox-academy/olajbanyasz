5fd7aab6feafb86dfae8d0aa225c382d5fb0b4e5
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2015-11-27 09:21:51 +0100

    practice till 8

--- week-3/5-kata/practice/practice.py
+++ week-3/5-kata/practice/practice.py
@@ -8,21 +8,46 @@ c = 44
 # please double c's value
 
 d = 125
-# please half d's value
+# please divide by 5 d's value
 
 e = 145673456
 # tell if it has 7 as divisor
 
-f = 1357988018575474 
+f1 = 123
+f2 = 345
+# tell if f1 is bigger than f2
+
+g1 = 350
+g2 = 200
+# tell if the double of g2 is bigger than g1
+
+h = 1357988018575474 
 # tell if it has 11 as a divisor
 
-g = [2134, 3, 432, 543, 134, 5435]
+i = [2134, 3, 432, 543, 134, 5435]
 # add the last and the first element
 
-h = [54, 23, 66, 12]
+j = [54, 23, 66, 12]
 # add the second a nd the third
 
-i = [1, 2, 3, 4, 5]
+k = [1, 2, 3, 4, 5]
 # increment the 3rd element
+
+l1 = 124
+l2 = 456
+# tell if l1 and l2 are both even numbers
+
+m1 = 203
+m2 = 97
+# tell if any of m1 or m2 is bigger than 100
+
+n = 123
+# print 'Hooray!' if the number is bigger than 100
+
+o = 24
+out = 0
+# if o is even increment out by one
+
+
 
 
