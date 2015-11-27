f324c34dbab2b2bd20f5e7febf5ee3210d26b7e7
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2015-11-27 08:37:58 +0100

    practice first 4 solution

--- week-3/5-kata/practice/practice.py
+++ week-3/5-kata/practice/practice.py
@@ -4,19 +4,25 @@ a = 3
 b = 100
 # make it smaller by 7
 
-c = 145673456
+c = 44
+# please double c's value
+
+d = 125
+# please half d's value
+
+e = 145673456
 # tell if it has 7 as divisor
 
-d = 1357988018575474 
+f = 1357988018575474 
 # tell if it has 11 as a divisor
 
-e = [2134, 3, 432, 543, 134, 5435]
+g = [2134, 3, 432, 543, 134, 5435]
 # add the last and the first element
 
-f = [54, 23, 66, 12]
+h = [54, 23, 66, 12]
 # add the second a nd the third
 
-d = [1, 2, 3, 4, 5]
+i = [1, 2, 3, 4, 5]
 # increment the 3rd element
 
 
