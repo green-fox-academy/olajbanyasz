2a5837b865f844446d7a2e4b08698c07529cbeb1
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2015-11-27 08:15:29 +0100

    add practice 1

---
+++ week-3/5-kata/practice/practice.py
@@ -0,0 +1,22 @@
+a = 3
+# make it bigger by 10
+
+b = 100
+# make it smaller by 7
+
+c = 145673456
+# tell if it has 7 as divisor
+
+d = 1357988018575474 
+# tell if it has 11 as a divisor
+
+e = [2134, 3, 432, 543, 134, 5435]
+# add the last and the first element
+
+f = [54, 23, 66, 12]
+# add the second a nd the third
+
+d = [1, 2, 3, 4, 5]
+# increment the 3rd element
+
+
