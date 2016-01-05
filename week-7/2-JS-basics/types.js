6507b20c4b24e9fae98e620e2516456fc2f8be47
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-01-05 10:24:20 +0100

    add variables

--- week-7/2-JS-basics/types.js
+++ week-7/2-JS-basics/types.js
@@ -1,37 +1,29 @@
-// one line comment
-/*
-Multi
-line
-comment
-*/
-
-
 // numbers
-1
--1
-12.23
-12341234123412341234
-1e3
-0b11
-011
-0x11
+1;
+-1;
+12.23;
+12341234123412341234;
+1e3;
+0b11;
+011;
+0x11;
 
-1 + 2
-2 - 1
-4 * 3
-2 / 3
-5 & 3
+1 + 2;
+2 - 1;
+4 * 3;
+2 / 3;
+5 & 3;
 
-1 / 0
--1 / 0
-0 / 0
-Infinity
--Infinity
-NaN
-Infinity + 123
-NaN / 12
-Infinity - Infinity
-NaN * NaN
+1 / 0;
+-1 / 0;
+0 / 0;
+Infinity;
+-Infinity;
+NaN;
+Infinity + 123;
+NaN / 12;
+Infinity - Infinity;
+NaN * NaN;
 
 // write 1024 in decimal, binary, octal and hexadecimal
 
@@ -44,49 +36,49 @@ NaN * NaN
 
 
 // Strings
-'Monkey'
-"Goat"
-"don't"
-'He is sooo "smart"...'
-'don\'t'
-'apple' + 'tree'
-'apple' - 'tree'
-'apple' * 3
+'Monkey';
+"Goat";
+"don't";
+'He is sooo "smart"...';
+'don\'t';
+'apple' + 'tree';
+'apple' - 'tree';
+'apple' * 3;
 
 // write in a string: don't be sooo "smart"
 
 
 // typeof
-typeof 123
-typeof 'apple'
-typeof Infinity
-typeof NaN
+typeof 123;
+typeof 'apple';
+typeof Infinity;
+typeof NaN;
 
 // automated casting
-'1' + 1
-'1' - 1
+'1' + 1;
+'1' - 1;
 
 // What is: '     12  '  * 12
 
 // Boolean
-true
-false
-!true
-false && true
-true || false
+true;
+false;
+!true;
+false && true;
+true || false;
 
 // Comparison
-1 < 2
-'a' > 'b'
-true > false
-12 <= 12
-12 >= 12
-13 == 13
-12 != 13
-'12' == 12
-1 == true
-1 === true
-3 !== 3
+1 < 2;
+'a' > 'b';
+true > false;
+12 <= 12;
+12 >= 12;
+13 == 13;
+12 != 13;
+'12' == 12;
+1 == true;
+1 === true;
+3 !== 3;
 
 
 
