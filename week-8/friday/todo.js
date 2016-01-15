96ae0d60b758be90256d7242efb079f8c46d3d0e
André Zsolt <andrezsolt71@gmail.com>
2016-01-15 12:17:12 +0100

    todo app in js

---
+++ week-8/friday/todo.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var todoContainer = document.querySelector('.todocontainer');
+var url = 'https://mysterious-dusk-8248.herokuapp.com/todos';
+var listButton = document.querySelector('.list-todos');
+var createButton = document.querySelector('.create-new-todo');
+var inputText = document.querySelector('textarea');
+listButton.addEventListener('click', callCreateRequest);
+createButton.addEventListener('click', callCreateNewToDo);
+
+function callCreateNewToDo() {
+  if (inputText.value != '') {
+    createNewTodo(inputText.value);
+    callCreateRequest();
+  }
+}
+
+function callCreateRequest() {
+  createRequest(displayTodos);
+}
+
+function callDeleteTodo() {
+  deleteTodo(event.target.parentNode.id);
+  callCreateRequest();
+}
+
+function callCompleteTodo() {
+  var textLength = (event.target.parentNode.innerText).length;
+  var subText = event.target.parentNode.innerText.substring(0, textLength-14);
+  completeTodo(event.target.parentNode.id, subText);
+  callCreateRequest();
+}
+
+function createRequest(callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.send();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      displayTodos(request.response);
+    }
+  }
+}
+
+function clearTodosFromDisplay() {
+  while (todoContainer.firstChild) {
+    todoContainer.removeChild(todoContainer.firstChild);
+  }
+}
+
+function displayTodos(response) {
+  clearTodosFromDisplay();
+  var todoArray = JSON.parse(response);
+  todoArray.forEach(function(todoItem) {
+    var newTodoItem = document.createElement('div');
+    newTodoItem.setAttribute('id', todoItem.id);
+    newTodoItem.innerText = todoItem.text;
+    todoContainer.appendChild(newTodoItem);
+    newTodoItem.addEventListener('click', createDeleteAndCompleteButtons);
+    newTodoItem.setAttribute('class', todoItem.completed)
+  });
+}
+
+function createDeleteAndCompleteButtons() {
+  deletePreviewsButtons();
+  makeCompleteButton();
+  makeDeleteButton();
+}
+
+function deletePreviewsButtons() {
+  var deleteButton = document.querySelector('.delete-button');
+  if (deleteButton) {
+  document.querySelector('.delete-button').remove();
+  document.querySelector('.complete-button').remove();
+  }
+}
+
+function makeCompleteButton() {
+  var newCompleteButton = document.createElement('button');
+  newCompleteButton.setAttribute('class', 'complete-button');
+  newCompleteButton.innerText = 'Complete';
+  event.target.appendChild(newCompleteButton);
+  newCompleteButton.addEventListener('click', callCompleteTodo);
+}
+
+function makeDeleteButton() {
+  var newDeleteButton = document.createElement('button');
+  newDeleteButton.innerText = 'Delete';
+  newDeleteButton.setAttribute('class', 'delete-button');
+  event.target.appendChild(newDeleteButton);
+  newDeleteButton.addEventListener('click', callDeleteTodo);
+}
+
+function createNewTodo(todotext) {
+  var request = new  XMLHttpRequest();
+  request.open('POST', url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({text: todotext}));
+}
+
+function completeTodo(id, desc) {
+  var request = new  XMLHttpRequest();
+  request.open('PUT', url + '/' + id);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({'text': desc, completed: 'true'}));
+}
+
+function deleteTodo(id) {
+  var request = new  XMLHttpRequest();
+  request.open('DELETE', url + '/' + id);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+}
+
+createRequest(displayTodos);
