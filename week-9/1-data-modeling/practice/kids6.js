ac5a46914fbf3eb69ae383031f50411620b543f2
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-01-19 08:37:36 +0100

    add loop practice

---
+++ week-9/1-data-modeling/practice/kids6.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var kids = [
+  {name: 'Julika', age: 8, sex: 'female'},
+  {name: 'Tiborka', age: 7, sex: 'male'},
+  {name: 'Zsolti', age: 6, sex: 'male'},
+  {name: 'Gerda', age: 9, sex: 'female'},
+  {name: 'Zsomborka', age: 8, sex: 'male'}
+];
+
+
+
+console.log(countBySex(kids)); // {female: 2, male: 3}
+
---
+++ week-9/1-data-modeling/practice/kids7.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var kids = [
+  {name: 'Julika', age: 8, sex: 'female'},
+  {name: 'Tiborka', age: 7, sex: 'male'},
+  {name: 'Zsolti', age: 6, sex: 'male'},
+  {name: 'Gerda', age: 9, sex: 'female'},
+  {name: 'Zsomborka', age: 8, sex: 'male'}
+];
+
+
+
+console.log(groupBySex(kids));
+/*
+{
+  female: [
+    {name: Julika, age: 8, sex}
+  ]
+}
+* /
+
